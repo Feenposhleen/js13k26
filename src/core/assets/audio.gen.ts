4311daf68213400b4e35df36cacf00b80c6e9b30
Charlo <chanor101@gmail.com>
2026-08-16 02:11:35 +0200

    Music pass

--- src/core/assets/audio.gen.ts
+++ src/core/assets/audio.gen.ts
@@ -5,7 +5,7 @@ export type RawSong = {
   _tracks: string[][];
 };
 
-const data = /*GEN*/{ _sfx: { _kick: '+28(0+05', _snare: '*4()/)>3', _bass: '*,(-@+,5', _beep: ')\\3**+64', _boop: '1C(#(162', _bang: '24(1N1.3', _laser: '3a( !1<4', _coin: '1N5%#1F3' }, _songs: { _main: { _bpm: 136, _tracks: [['_kick', '(.........*.....'], ['_snare', '....(.......,...'], ['_bass', '..(...,.........'], ['_beep', '(.*.(.(.(.*.*.(*']] } } }/*/GEN*/;
+const data = /*GEN*/{ _sfx: { _kick: '(+-(.)HA', _snare: ',9I(-+>3', _bass: '+1.(N)8+', _beep: '*.;*5*6-', _boop: '1C(#(162', _bang: '24(1N1.3', _laser: '3a( !1<4', _coin: '1N5%#1F3', _hihat: ')0.(2*@F' }, _songs: { _main: { _bpm: 120, _tracks: [['_kick', '(.(.....(.......'], ['_snare', '....(.......(...'], ['_bass', '.,.(..*......,,.'], ['_hihat', '(((,*.,.(.(.(.**']] } } }/*/GEN*/;
 
 export type RawAudioData = typeof data;
 
